f706f23a2fac323e07aa2d4af77872a004209a3d
Eugene Shatsky <eugene@shatsky.net>
2020-11-06 07:45:22 +0200

    Add desktop file for niudu-devices

--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ setup(name='niudu',
       version='0.0.1',
       packages=['niudu_devices', 'niudu_devices.subsystems', 'niudu_devices.plugins'],
       scripts=['niudu-devices'],
-      data_files=[('share/niudu-devices/icons', glob.glob('niudu_devices/icons/*')), ('share/niudu-devices/hwdata', glob.glob('niudu_devices/hwdata/*'))]
+      data_files=[('share/niudu-devices/icons', glob.glob('niudu_devices/icons/*')), ('share/niudu-devices/hwdata', glob.glob('niudu_devices/hwdata/*')),
+                  ('share/applications', ['niudu-devices.desktop'])
+                  ]
       )
