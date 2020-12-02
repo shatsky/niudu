aa92943e7d8ab529b9f770767b19ccafbc0c534f
Eugene Shatsky <eugene@shatsky.net>
2020-12-02 20:25:12 +0200

    move niudu-devices test to tests/

--- tests/test_niudu_devices.py
+++ tests/test_niudu_devices.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 
 from PySide2 import QtWidgets, QtGui
 
@@ -6,8 +7,10 @@ from PySide2 import QtWidgets, QtGui
 class MyTestCase(unittest.TestCase):
 
     def test_iterating_devices_props(self):
-        from ..ui_devices_view import iter_devices
-        from ..device import iter_props_tree_items, update_dict
+        import niudu_devices
+        niudu_devices.DATA_PATH = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../share/niudu-devices'))
+        from niudu_devices.ui_devices_view import iter_devices
+        from niudu_devices.device import iter_props_tree_items, update_dict
         for device_path in iter_devices():
             device_dict = {}
             update_dict(device_path, device_dict)
